9c3cc89b047799dd6ea5ffd95c7f6903eeab090e
Salvador Guzman <guzmansalv@gmail.com>
2019-04-20 23:24:52 -0700

    change schema

--- schema.sql
+++ schema.sql
@@ -1,38 +1,38 @@
 CREATE database youtube;
 \connect youtube
-CREATE SCHEMA entities;
+CREATE SCHEMA stats;
 
-CREATE TABLE youtube.entities.channels(
+CREATE TABLE youtube.stats.channels(
   serial CHAR(24) PRIMARY KEY NOT NULL
 );
-CREATE UNIQUE INDEX channels_serial_uindex ON youtube.entities.channels (serial);
+CREATE UNIQUE INDEX channels_serial_uindex ON youtube.stats.channels (serial);
 
-CREATE TABLE youtube.entities.chan_subs
+CREATE TABLE youtube.stats.chan_subs
 (
   time timestamptz DEFAULT now() NOT NULL,
   serial char(24) NOT NULL,
   subs bigint NOT NULL
 );
 
-SELECT create_hypertable('youtube.entities.chan_subs', 'time', 'serial', 32767);
+SELECT create_hypertable('youtube.stats.chan_subs', 'time', 'serial', 32767);
 
-CREATE TABLE youtube.entities.chan_videos
+CREATE TABLE youtube.stats.chan_videos
 (
   time timestamptz DEFAULT now() NOT NULL,
   serial char(24) NOT NULL,
   videos int NOT NULL
 );
 
-SELECT create_hypertable('youtube.entities.chan_videos', 'time', 'serial', 32767);
+SELECT create_hypertable('youtube.stats.chan_videos', 'time', 'serial', 32767);
 
-CREATE TABLE youtube.entities.chan_views
+CREATE TABLE youtube.stats.chan_views
 (
   time timestamptz DEFAULT now() NOT NULL,
   serial char(24) NOT NULL,
   views bigint NOT NULL
 );
 
-SELECT create_hypertable('youtube.entities.chan_views', 'time', 'serial', 32767);
+SELECT create_hypertable('youtube.stats.chan_views', 'time', 'serial', 32767);
 
 create view space_usage as SELECT *, pg_size_pretty(total_bytes) AS total
  , pg_size_pretty(index_bytes) AS INDEX
